2b879f98c506178b8db371861a999aff5c5bf620
Maarten Smeets <wisp1980@gmail.com>
2018-01-21 13:59:05 +0100

    Adding sample code

---
+++ src/main/java/nl/amis/smeetsm/springboot/MicroserviceDemo.java
@@ -0,0 +1,14 @@
+package nl.amis.smeetsm.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDemo.class, args);
+
+	}
+
+}
---
+++ src/main/java/nl/amis/smeetsm/springboot/topic/TopicService.java
@@ -0,0 +1,43 @@
+package nl.amis.smeetsm.springboot.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+//singleton
+@Service
+public class TopicService {
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring","Framework","FrameworkDescription"),
+			new Topic("java","core Java","JavaDescription"),
+			new Topic("javascript","Javascript","JavascriptDescription")));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for(int i=0;i<topics.size();i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
