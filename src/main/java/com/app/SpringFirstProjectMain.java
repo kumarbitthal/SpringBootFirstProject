3a1f0ef8b448a448e37a78a0ac1b69bfa4eaa8d2
Kumar Bitthal <kumarbitthal212@gmail.com>
2020-06-28 23:35:25 +0530

    swagger imported

--- src/main/java/com/app/SpringFirstProjectMain.java
+++ src/main/java/com/app/SpringFirstProjectMain.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({" com.topic", "com.exceptions"})
+@ComponentScan({" com.topic", "com.exceptions","com.documents"})
 public class SpringFirstProjectMain {
 
 	public static void main(String[] args) {
